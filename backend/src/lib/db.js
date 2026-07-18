7ccfb20b86a53ed2e4c6d144165fa7cdd1905a8d
premdev <prempravash65@gmail.com>
2026-07-18 10:30:00 +0530

    Update db.js

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,15 +1,13 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
-import dns from 'dns';
-dns.setServers([
-  '1.1.1.1','8.8.8.8'
-])
+import dns from 'dns'
+dns.setServers(['1.1.1.1', '8.8.8.8'])
 export const connectDB = async () => {
   try {
-    const uri = process.env.MONGODB_URI;
-    const conn = await mongoose.connect(uri);
-    console.log("db-connected")
+    const uri = process.env.MONGODB_URI
+    const conn = await mongoose.connect(uri)
+    console.log('db-connected')
   } catch (e) {
-    console.log("Error : " , e.message);
+    console.log('Error : ', e.message)
   }
-};
+}
