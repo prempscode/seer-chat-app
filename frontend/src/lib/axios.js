8f7309b2c666c80c497ac28777984f5a3344ca0d
premdev <prempravash65@gmail.com>
2026-07-31 10:30:00 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,9 +1,8 @@
-import axios from "axios";
-
+import axios from 'axios'
 
 const api = axios.create({
-  baseURL: "/api",
-  withCredentials: true, 
-});
+  baseURL: '/api',
+  withCredentials: true
+})
 
-export default api;
+export default api
